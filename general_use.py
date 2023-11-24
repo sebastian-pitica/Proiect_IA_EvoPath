50d378e0a31ef75820f6d27f76a8af3a9791cc87
Sebastian Razvan <74203905+sebastian-pitica@users.noreply.github.com>
2023-11-24 18:13:04 +0200

    updates

---
+++ general_use.py
@@ -0,0 +1,15 @@
+# /**************************************************************************
+#
+#  File:        general_use.py
+#  Copyright:   (c) 2023 EvoPath Team
+#  Description:
+#  Designed by: Sebastian Pitica
+#
+#  Module-History:
+#  Date        Author                Reason
+#  24.11.2023  Sebastian Pitica      Basic structure
+#
+#  **************************************************************************/
+
+maze = list(list())
+populations = list(list())
